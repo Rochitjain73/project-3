c630c9cedff576008cb0070a9464b3c46041bc6c
rochit jain <rochitsethiya@gmail.com>
2025-07-30 15:05:26 +0530

    change hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -80,7 +80,7 @@ export default function Hero() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.7 }}
           >
-            "Turning smart ideas into real-world AI tools."
+            "Turning smart ideas into real-world Project."
           </motion.p>
           
           <motion.div
