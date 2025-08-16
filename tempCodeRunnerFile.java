8be11a2d9747bfd9c482d6c2e809a34be0c55ddc
rochit jain <rochitsethiya@gmail.com>
2025-08-16 14:12:13 +0530

    Update Projects section with new details

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,13 @@
+import java.util.*;
+    public class condition {
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            if (x%2 == 0){
+            System.out.println("X is even number");
+            }else{
+            System.out.println("X is odd number ");
+            }
+        }
+
+    }
